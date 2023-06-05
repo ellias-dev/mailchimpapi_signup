07ca0f9fcf5ae4215f18a5c453724b2e9cb26662
ellias <info@ellias.dev>
2023-06-05 22:29:03 +0400

    document mailchimp module

--- mailchimp.js
+++ mailchimp.js
@@ -1,12 +1,12 @@
 const sanitizeHtml = require('sanitize-html');
 
 const mailchimp = (req, res) => {
+  //Declare vars storing user input and sanitize them
     const firstName = sanitizeHtml(req.body.fname);
     const lastName = sanitizeHtml(req.body.lname);
     const email = sanitizeHtml(req.body.email);
 
-    console.log(firstName, lastName, email);
-    
+    //Mailchimp API section
     const client = require("@mailchimp/mailchimp_marketing");
       client.setConfig({
         apiKey: process.env.MAILCHIMP_API,
@@ -18,13 +18,13 @@ const mailchimp = (req, res) => {
         const response = await client.lists.batchListMembers(listId, {
           members: [{
             email_address: email,
-            status: 'pending',
+            status: 'pending', //use 'pending' if you want users to confirm emails, otherwise replace it with 'subscribed'
             merge_fields: {
               FNAME: firstName,
               LNAME: lastName
           }}],
         });
-        console.log(response.total_created, response.error_count);
+        //console.log(response.total_created, response.error_count);
         
         if(response.total_created > 0){
           res.sendFile(__dirname + '/success.html');
